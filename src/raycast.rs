46625b935048109475718bc9f49863e09df3d242
Shaen <shane@siler.io>
2021-03-09 17:20:08 -0800

    well.

--- src/raycast.rs
+++ src/raycast.rs
@@ -12,7 +12,7 @@ pub enum RaycastAlgorithm {
 
 impl Default for RaycastAlgorithm {
     fn default() -> Self {
-        RaycastAlgorithm::Geometric
+        RaycastAlgorithm::MollerTrumbore(Backfaces::Include)
     }
 }
 
